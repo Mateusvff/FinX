ee2ed99ab58286ec99478f791a3129489dcf2b11
Arthur Gabriel <78980894+arthurgabriellg@users.noreply.github.com>
2021-03-30 20:53:24 -0300

    V1.013
    Colocando botão ao container 4

--- lib/home.dart
+++ lib/home.dart
@@ -14,11 +14,13 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( // Scaffold a ser buildado
+    return Scaffold(
+      // Scaffold a ser buildado
       backgroundColor: customBg,
-      appBar: AppBar( // Barra Inicial
+      appBar: AppBar(
+        // Barra Inicial
         title: Text(
-          'Seja Bem Vindo,\n Carlos',
+          'Seja Bem Vindo,\n xxxx', // o nome sera pego pelo firebase
           style: TextStyle(
             fontSize: 16.0,
           ),
@@ -32,11 +34,14 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      body: SingleChildScrollView( // Scroll
-        child: Column( // Coluna Principal
+      body: SingleChildScrollView(
+        // Scroll
+        child: Column(
+          // Coluna Principal
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container( // Primeiro Container (PRINCIPAL)
+            Container(
+              // Primeiro Container (PRINCIPAL)
               margin: const EdgeInsets.fromLTRB(5.0, 25.0, 5.0, 25.0),
               padding: const EdgeInsets.fromLTRB(190.0, 120.0, 190.0, 120.0),
               decoration: BoxDecoration(
@@ -44,21 +49,24 @@ class _HomeState extends State<Home> {
                   borderRadius: const BorderRadius.all(Radius.circular(25))),
               child: Column(),
             ),
-            Container( // Segundo Container (INFO CARTOES)
+            Container(
+              // Segundo Container (INFO CARTOES)
               margin: const EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
               padding: const EdgeInsets.fromLTRB(15.0, 60.0, 15.0, 60.0),
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: const BorderRadius.all(Radius.circular(25))),
             ),
-            Container( // Terceiro Container (EXTRATO DA CONTA BANCARIA)
+            Container(
+              // Terceiro Container (EXTRATO DA CONTA BANCARIA)
               margin: const EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
               padding: const EdgeInsets.fromLTRB(15.0, 60.0, 15.0, 60.0),
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: const BorderRadius.all(Radius.circular(25))),
             ),
-            Container( // Quarto Container (CARTOES/EXTRATO/CATEGORIAS)
+            Container(
+              // Quarto Container (CARTOES/EXTRATO/CATEGORIAS)
               margin: const EdgeInsets.fromLTRB(55.0, 20.0, 55.0, 20.0),
               padding: const EdgeInsets.fromLTRB(5.0, 20.0, 5.0, 20.0),
               decoration: BoxDecoration(
@@ -69,33 +77,38 @@ class _HomeState extends State<Home> {
                   Expanded(
                     child: Container(
                       alignment: Alignment.center,
-                      child: Text(
-                        'Cartões',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
+                      child: TextButton(
+                        child: Text('Cartões',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            )),
+                        onPressed: () {},
                       ),
                     ),
                   ),
                   Expanded(
                     child: Container(
                       alignment: Alignment.center,
-                      child: Text(
-                        'Extrato',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
+                      child: TextButton(
+                        child: Text('Extrato',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            )),
+                        onPressed: () {},
                       ),
                     ),
                   ),
                   Expanded(
                     child: Container(
                       alignment: Alignment.center,
-                      child: Text(
-                        'Categorias',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                      child: TextButton(
+                        child: Text(
+                          'Categorias',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
+                        onPressed: () {},
                       ),
                     ),
                   ),
