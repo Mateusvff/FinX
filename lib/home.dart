ae207858a9a6f725e681cd7400496bac0c2d754a
Caio-bergbjj <caioberg09@gmail.com>
2021-03-31 00:34:50 -0300

    V1.019
    Colocando o SizedBox e arrumando o Padding

--- lib/home.dart
+++ lib/home.dart
@@ -40,115 +40,147 @@ class _HomeState extends State<Home> {
           // Coluna Principal
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              // Primeiro Container (PRINCIPAL)
-              child: Stack(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(5.0, 25.0, 5.0, 25.0),
-                    padding:
-                        const EdgeInsets.fromLTRB(190.0, 120.0, 190.0, 120.0),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(25))),
-                    child: Column(
-                      children: [],
-                    ),
-                  ),
-                  Positioned.fill(
-                      child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: CircleAvatar(
-                      backgroundColor: customPurple,
-                      radius: 30.0,
-                      child: IconButton(
-                        icon: Icon(Icons.add),
-                        iconSize: 40.0,
-                        color: Colors.white,
-                        onPressed: () {},
+            Padding(
+              padding: const EdgeInsets.fromLTRB(14.0, 20.0, 14.0, 47.0),
+              child: SizedBox(
+                width: 383,
+                height: 226,
+                child: Container(
+                  // Primeiro Container (PRINCIPAL)
+                  child: Stack(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(
+                            190.0, 120.0, 190.0, 120.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(25))),
+                        child: Column(
+                          children: [],
+                        ),
                       ),
-                    ),
-                  )),
-                  Positioned.fill(
-                      child: Align(
-                    alignment: Alignment.topLeft,
-                    child: CircleAvatar(
-                      backgroundColor:
-                          Colors.grey, // Adicionar Foto do Usuario (Firebase)
-                      radius: 30.0,
-                    ),
-                  )),
-                ],
-              ),
-            ),
-            Container(
-              // Segundo Container (INFO CARTOES)
-              margin: const EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
-              padding: const EdgeInsets.fromLTRB(15.0, 60.0, 15.0, 60.0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: const BorderRadius.all(Radius.circular(25))),
-            ),
-            Container(
-              // Terceiro Container (EXTRATO DA CONTA BANCARIA)
-              margin: const EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
-              padding: const EdgeInsets.fromLTRB(15.0, 60.0, 15.0, 60.0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: const BorderRadius.all(Radius.circular(25))),
-            ),
-            Container(
-              // Quarto Container (CARTOES/EXTRATO/CATEGORIAS)
-              margin: const EdgeInsets.fromLTRB(55.0, 20.0, 55.0, 20.0),
-              padding: const EdgeInsets.all(5.0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: const BorderRadius.all(Radius.circular(25))),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: TextButton(
-                        child: Text('Cartões',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            )),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: TextButton(
-                        child: Text('Extrato',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            )),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: TextButton(
-                        child: Text(
-                          'Categorias',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
+                      Positioned.fill(
+                          child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: CircleAvatar(
+                          backgroundColor: customPurple,
+                          radius: 30.0,
+                          child: IconButton(
+                            icon: Icon(Icons.add),
+                            iconSize: 40.0,
+                            color: Colors.white,
+                            onPressed: () {},
                           ),
                         ),
-                        onPressed: () {},
-                      ),
-                    ),
+                      )),
+                      Positioned.fill(
+                          child: Align(
+                        alignment: Alignment.topLeft,
+                        child: CircleAvatar(
+                          backgroundColor: Colors
+                              .grey, // Adicionar Foto do Usuario (Firebase)
+                          radius: 30.0,
+                        ),
+                      )),
+                    ],
                   ),
-                ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(14.0, 0.0, 14.0, 50.0),
+              child: SizedBox(
+                width: 383,
+                height: 112,
+                child: Container(
+                  // Segundo Container (INFO CARTOES)
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(25))),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(14.0, 0.0, 14.0, 71.0),
+              child: SizedBox(
+                width: 383,
+                height: 112,
+                child: Container(
+                  // Terceiro Container (EXTRATO DA CONTA BANCARIA)
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(25))),
+                  child: Column(
+                    children: <Widget>[
+                      Text('Extrato da conta (Nubank)'),
+                      Text('Mês'),
+                      Text('fjksdbj'),
+                      Text('bhfvsdb'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(14.0, 0.0, 14.0, 16.0),
+              child: SizedBox(
+                width: 300,
+                height: 50,
+                child: Container(
+                  // Quarto Container (CARTOES/EXTRATO/CATEGORIAS)
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(25))),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: TextButton(
+                            child: Text('Cartões',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                )),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: TextButton(
+                            child: Text('Extrato',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                )),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: TextButton(
+                            child: Text(
+                              'Categorias',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             )
           ],
