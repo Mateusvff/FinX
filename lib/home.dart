9e17ab767caddf22ec20cb0d5c7b1b116ad9edfa
Caio-bergbjj <caioberg09@gmail.com>
2021-03-31 01:11:18 -0300

    V1.02

--- lib/home.dart
+++ lib/home.dart
@@ -39,54 +39,51 @@ class _HomeState extends State<Home> {
         child: Column(
           // Coluna Principal
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(14.0, 20.0, 14.0, 47.0),
-              child: SizedBox(
-                width: 383,
-                height: 226,
-                child: Container(
-                  // Primeiro Container (PRINCIPAL)
-                  child: Stack(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(
-                            190.0, 120.0, 190.0, 120.0),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(25))),
-                        child: Column(
-                          children: [],
-                        ),
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(14.0, 20.0, 14.0, 47.0),
+                  child: SizedBox(
+                    width: 383.0,
+                    height: 226.0,
+                    child: Container(
+                      padding:
+                          const EdgeInsets.fromLTRB(190.0, 120.0, 190.0, 120.0),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(25))),
+                      child: Column(
+                        children: [],
                       ),
-                      Positioned.fill(
-                          child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: CircleAvatar(
-                          backgroundColor: customPurple,
-                          radius: 30.0,
-                          child: IconButton(
-                            icon: Icon(Icons.add),
-                            iconSize: 40.0,
-                            color: Colors.white,
-                            onPressed: () {},
-                          ),
-                        ),
-                      )),
-                      Positioned.fill(
-                          child: Align(
-                        alignment: Alignment.topLeft,
-                        child: CircleAvatar(
-                          backgroundColor: Colors
-                              .grey, // Adicionar Foto do Usuario (Firebase)
-                          radius: 30.0,
-                        ),
-                      )),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+                Positioned.fill(
+                    child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: CircleAvatar(
+                    backgroundColor: customPurple,
+                    radius: 30.0,
+                    child: IconButton(
+                      icon: Icon(Icons.add),
+                      iconSize: 40.0,
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ),
+                )),
+                Positioned.fill(
+                    child: Align(
+                  alignment: Alignment.topLeft,
+                  child: CircleAvatar(
+                    backgroundColor:
+                        Colors.grey, // Adicionar Foto do Usuario (Firebase)
+                    radius: 30.0,
+                  ),
+                )),
+              ],
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(14.0, 0.0, 14.0, 50.0),
@@ -115,7 +112,15 @@ class _HomeState extends State<Home> {
                           const BorderRadius.all(Radius.circular(25))),
                   child: Column(
                     children: <Widget>[
-                      Text('Extrato da conta (Nubank)'),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: Text(
+                            'Extrato da conta (Nubank)',
+                          ),
+                        ),
+                      ),
                       Text('Mês'),
                       Text('fjksdbj'),
                       Text('bhfvsdb'),
