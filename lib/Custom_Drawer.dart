018cc7ed35937cc968a501a3638322c2144253c2
Caio-bergbjj <caioberg09@gmail.com>
2021-04-01 20:11:16 -0300

    V1.046

--- lib/Custom_Drawer.dart
+++ lib/Custom_Drawer.dart
@@ -13,7 +13,20 @@ class CustomDrawer extends StatelessWidget {
         );
     return Drawer(
       child: Stack(
-        children: <Widget>[_buildDrawerBack()],
+        children: <Widget>[
+          _buildDrawerBack(),
+          ListView(
+            padding: EdgeInsets.only(left: 32.0),
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 8.0),
+                padding: EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 16.0),
+                height: 170.0,
+                child: Stack(),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
