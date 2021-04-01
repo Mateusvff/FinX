319c94f95b24e95d774b4f4b9bb7fac3749fb052
Mateus Franco <mateusfrancovinicius@gmail.com>
2021-04-01 17:56:44 -0300

    Tela de cadastro inicial

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -11,29 +11,39 @@ class _CadastroState extends State<Cadastro> {
     return Stack(
       children: <Widget>[
         Image.asset(
-          'assets/background.jpg',
+          'assets/fundo.jpg',
           fit: BoxFit.cover,
           height: 1000.0,
         ),
         Column(
           children: [
-            Text(
-              "FinX,",
-              style: TextStyle(
-                fontFamily: 'NotoSans',
-                color: Colors.deepPurpleAccent,
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 5),
+              child:
+              Text(
+                "FinX,",
+                style: TextStyle(
+                  fontSize: 45.0,
+                  fontFamily: 'NotoSans',
+                  color: Color(0xFF6725D6),
+                ),
               ),
             ),
-            Text(
-              "Escolhas inteligentes para o seu bolso",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontFamily: 'NotoSans',
-                color: Colors.white,
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child:
+              Text(
+                "Escolhas inteligentes para o seu bolso",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'NotoSans',
+                  color: Colors.white,
+
+                ),
               ),
             )
           ],
-
         )
       ],
     );
