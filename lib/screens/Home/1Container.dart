21ce6c084a24c6ce78563c4abd1f614a5367b87f
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2021-04-07 22:09:29 -0300

    V2.02
    scrollview container 3

--- lib/screens/Home/1Container.dart
+++ lib/screens/Home/1Container.dart
@@ -16,89 +16,92 @@ class _ExtratoMesState extends State<ExtratoMes> {
       decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: const BorderRadius.all(Radius.circular(25))),
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.arrow_back_ios_rounded),
-                color: Colors.black,
-                iconSize: 18,
-                onPressed: () {}, /* Mes Passado */
-              ),
-              Text(
-                'Mês atual',
-                /* Puxar de acordo com o mes atual */
-                style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
-              IconButton(
-                icon: Icon(Icons.arrow_forward_ios_rounded),
-                color: Colors.black,
-                iconSize: 18,
-                onPressed: () {}, /* Proximo Mes */
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 50.0),
-                child: Text(
-                  'Saldo em conta',
-                  style: TextStyle(color: Colors.black, fontSize: 14),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios_rounded),
+                  color: Colors.black,
+                  iconSize: 18,
+                  onPressed: () {}, /* Mes Passado */
                 ),
-              ),
-              IconButton(
-                alignment: Alignment.topRight,
-                icon: Icon(Icons.remove_red_eye_sharp),
-                color: Colors.black,
-                iconSize: 18,
-                onPressed:
-                    () {}, /* Tampar o saldo do usuario ou Mostrar o saldo do usuario*/
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('R\$10,000,00',
-                  /* Puxar do usuario de acordo com Receita - Despesa */
-                  style: TextStyle(color: Colors.black, fontSize: 18)),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Receitas ',
-                style: TextStyle(color: Colors.black, fontSize: 16),
-              ),
-              Text(
-                ' Despesas',
-                style: TextStyle(color: Colors.black, fontSize: 16),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'R\$15.000,00 ',
-                /* Puxar do Add Receitas */
-                style:
-                    TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'R\$5.000,00 ',
-                /* Puxar do Add Despesas */
-                style: TextStyle(color: customRed, fontWeight: FontWeight.bold),
-              )
-            ],
-          )
-        ],
+                Text(
+                  'Mês atual',
+                  /* Puxar de acordo com o mes atual */
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_forward_ios_rounded),
+                  color: Colors.black,
+                  iconSize: 18,
+                  onPressed: () {}, /* Proximo Mes */
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 50.0),
+                  child: Text(
+                    'Saldo em conta',
+                    style: TextStyle(color: Colors.black, fontSize: 14),
+                  ),
+                ),
+                IconButton(
+                  alignment: Alignment.topRight,
+                  icon: Icon(Icons.remove_red_eye_sharp),
+                  color: Colors.black,
+                  iconSize: 18,
+                  onPressed:
+                      () {}, /* Tampar o saldo do usuario ou Mostrar o saldo do usuario*/
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('R\$10,000,00',
+                    /* Puxar do usuario de acordo com Receita - Despesa */
+                    style: TextStyle(color: Colors.black, fontSize: 18)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Receitas ',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+                Text(
+                  ' Despesas',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'R\$15.000,00 ',
+                  /* Puxar do Add Receitas */
+                  style:
+                  TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'R\$5.000,00 ',
+                  /* Puxar do Add Despesas */
+                  style: TextStyle(color: customRed, fontWeight: FontWeight.bold),
+                )
+              ],
+            )
+          ],
+        ),
       ),
+
     );
   }
 }
