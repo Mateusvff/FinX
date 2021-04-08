db5af230b9cc7107d0460d3a42dd9cfdd9732cab
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2021-04-07 23:48:51 -0300

    V2.04
    Retirado o ScrollView do primeiro container
    Adicionado o ScrollView no segundo e no terceiro container

--- lib/screens/Home/1Container.dart
+++ lib/screens/Home/1Container.dart
@@ -17,43 +17,45 @@ class _ExtratoMesState extends State<ExtratoMes> {
       decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: const BorderRadius.all(Radius.circular(25))),
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.arrow_back_ios_rounded),
-                  color: Colors.black,
-                  iconSize: 18,
-                  onPressed: () {}, /* Mes Passado */
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.arrow_back_ios_rounded),
+                color: Colors.black,
+                iconSize: 18,
+                onPressed: () {}, /* Mes Passado */
+              ),
+              Text(
+                'Mês atual',
+                /* Puxar de acordo com o mes atual */
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward_ios_rounded),
+                color: Colors.black,
+                iconSize: 18,
+                onPressed: () {}, /* Proximo Mes */
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 50.0),
+                child: Text(
+                  'Saldo em conta',
+                  style: TextStyle(color: Colors.black, fontSize: 14),
                 ),
-                Text(
-                  'Mês atual',
-                  /* Puxar de acordo com o mes atual */
-                  style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                IconButton(
-                  icon: Icon(Icons.arrow_forward_ios_rounded),
-                  color: Colors.black,
-                  iconSize: 18,
-                  onPressed: () {}, /* Proximo Mes */
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 50.0),
-                  child: Text(
-                    'Saldo em conta',
-                    style: TextStyle(color: Colors.black, fontSize: 14),
-                  ),
-                ),
-                Padding(
-                    padding:EdgeInsets.only(top:15.0),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15.0),
                 child: IconButton(
                   alignment: Alignment.topRight,
                   icon: Icon(Icons.remove_red_eye_sharp),
@@ -62,64 +64,64 @@ class _ExtratoMesState extends State<ExtratoMes> {
                   onPressed:
                       () {}, /* Tampar o saldo do usuario ou Mostrar o saldo do usuario*/
                 ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('R\$10,000,00',
+                  /* Puxar do usuario de acordo com Receita - Despesa */
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 60.0, top: 10.0),
+                child: Text(
+                  'Receitas ',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                 Text('R\$10,000,00',
-                    /* Puxar do usuario de acordo com Receita - Despesa */
-                    style: TextStyle(color: Colors.black, fontSize: 26, fontWeight:FontWeight.bold )
-                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                 Padding(
-                    padding: const EdgeInsets.only(right: 60.0, top: 10.0),
-                    child: Text(
-                      'Receitas ',
-                      style: TextStyle(color: Colors.black, fontSize: 16),
-                    ),
-                  ),
-               Padding(
-                   padding: EdgeInsets.only(top: 10.0, left: 60.0),
-                  child: Text(
-                 ' Despesas',
-                 style: TextStyle(color: Colors.black, fontSize: 16),
-               ),
-               ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                    padding: EdgeInsets.only(right: 50.0, top: 5.0),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, left: 60.0),
+                child: Text(
+                  ' Despesas',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 50.0, top: 5.0),
                 child: Text(
                   'R\$15.000,00 ',
                   /* Puxar do Add Receitas */
-                  style:
-                  TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                      color: Colors.green, fontWeight: FontWeight.bold),
                 ),
-                ),
-
-                Padding(
-                    padding: EdgeInsets.only(top: 5.0,left: 50.0),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, left: 50.0),
                 child: Text(
                   'R\$5.000,00 ',
                   /* Puxar do Add Despesas */
-                  style: TextStyle(color: customRed, fontWeight: FontWeight.bold),
+                  style:
+                      TextStyle(color: customRed, fontWeight: FontWeight.bold),
                 ),
-                ),
-              ],
-            )
-          ],
-        ),
+              ),
+            ],
+          )
+        ],
       ),
-
     );
   }
 }
--- lib/screens/Home/3Container.dart
+++ lib/screens/Home/3Container.dart
@@ -15,49 +15,51 @@ class _ExtratoBancoState extends State<ExtratoBanco> {
       decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: const BorderRadius.all(Radius.circular(25))),
-      child: Column(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20.0, top: 5.0),
-              child: Text(
-                'Extrato da conta (Banco)',
-                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 5.0),
+                child: Text(
+                  'Extrato da conta (Banco)',
+                  style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20.0, top: 5.0),
-              child: Text(
-                'Mês atual',
-                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 5.0),
+                child: Text(
+                  'Mês atual',
+                  style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20.0, top: 5.0),
-              child: Text(
-                '- Dia(xx/xx): pagamento/recebido :R\$ xxx,xx(valor da transação)',
-                style: TextStyle(fontSize: 12.0),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 5.0),
+                child: Text(
+                  '- Dia(xx/xx): pagamento/recebido :R\$ xxx,xx(valor da transação)',
+                  style: TextStyle(fontSize: 12.0),
+                ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20.0, top: 5.0),
-              child: Text(
-                '- Dia(xx/xx): pagamento/recebido :R\$ xxx,xx(valor da transação)',
-                style: TextStyle(fontSize: 12.0),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 5.0),
+                child: Text(
+                  '- Dia(xx/xx): pagamento/recebido :R\$ xxx,xx(valor da transação)',
+                  style: TextStyle(fontSize: 12.0),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
