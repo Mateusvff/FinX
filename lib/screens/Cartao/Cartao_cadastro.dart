6afb8b764d035c48ca4b22251b9c35dda1c13a50
Caio-bergbjj <caioberg09@gmail.com>
2021-04-16 01:32:35 -0300

    V2.47

--- lib/screens/Cartao/Cartao_cadastro.dart
+++ lib/screens/Cartao/Cartao_cadastro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_flutter/Cores.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class CartaoCad extends StatefulWidget {
   @override
@@ -22,20 +23,65 @@ class _CartaoCadState extends State<CartaoCad> {
           ),
         ),
       ),
-      body: Column(
-        children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Positioned(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 40, bottom: 8),
+              child: Container(
                 child: Icon(
-                  Icons.credit_card_outlined,
+                  MdiIcons.creditCardPlusOutline,
                   color: Colors.grey,
-                  size: 300,
+                  size: 250,
                 ),
               ),
-            ],
-          )
-        ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 5, top: 30, bottom: 20),
+              child: TextFormField(
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                textAlign: TextAlign.left,
+                style: TextStyle(color: Colors.white, fontSize: 15),
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.pink,
+                    ),
+                  ),
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+                  ),
+                  labelText: "Limite de Crédito (R\$)",
+                  labelStyle: TextStyle(color: Colors.grey, fontSize: 20),
+                ),
+              ),
+            ),
+            Container(
+              height: 440,
+              width: 410,
+              decoration: BoxDecoration(
+                  color: customPurple,
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(22.0),
+                    topLeft: Radius.circular(22.0),
+                  )),
+              child: Text(
+                'data',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
