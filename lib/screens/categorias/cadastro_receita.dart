6fe307fe27af8d0d7c5d1dcf01a236ccdb0b8786
Arthur Gabriel <78980894+arthurgabriellg@users.noreply.github.com>
2021-04-22 01:11:02 -0300

    pagina de receitas

--- lib/screens/categorias/cadastro_receita.dart
+++ lib/screens/categorias/cadastro_receita.dart
@@ -16,7 +16,7 @@ class _CadastroReceitaState extends State<CadastroReceita> {
         elevation: 0,
         backgroundColor: customBg,
         title: Text(
-          'Cadastro de receita',
+          'Adicionar receita',
           style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.white,
@@ -24,18 +24,59 @@ class _CadastroReceitaState extends State<CadastroReceita> {
         ),
       ),
       body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Text(
-            'Valor recebido',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 40,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 220.0, right: 180.0),
+              child: Text(
+                'Valor recebido',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
             ),
-          )
-        ],
-      )),
+            Padding(
+              padding: const EdgeInsets.only(right: 130.0),
+              child: Text(
+                'R\$1000',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 60,
+                ),
+              ),
+            ),
+            Container(
+              width: 410,
+              height: 440,
+              decoration: BoxDecoration(
+                color: customPurple,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(22.0),
+                  topLeft: Radius.circular(22.0),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 80.0, right: 290.0),
+                    child: Text(
+                      'Data',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
