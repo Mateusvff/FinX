a5572f8413a2b46a71d796b18a2b31312df95d74
Arthur Gabriel <78980894+arthurgabriellg@users.noreply.github.com>
2021-04-22 00:06:43 -0300

    v2.5

--- lib/Widgets/toggleButtons.dart
+++ lib/Widgets/toggleButtons.dart
@@ -11,44 +11,40 @@ class _ToggleButtonState extends State<ToggleButton> {
   List<bool> _isSelected = [true, false];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: customPurple,
-      child: ToggleButtons(
-        isSelected: _isSelected,
-        fillColor: customPink,
-        renderBorder: false,
-        borderWidth: 10,
-        borderRadius: BorderRadius.circular(40),
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0),
-            child: Text('Receitas',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0),
-            child: Text('Despesas',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                )),
-          ),
-        ],
-        onPressed: (int newIndex) {
-          setState(() {
-            for (int index = 0; index < _isSelected.length; index++) {
-              if (index == newIndex) {
-                _isSelected[index] = true;
-              } else {
-                _isSelected[index] = false;
-              }
+    return ToggleButtons(
+      isSelected: _isSelected,
+      fillColor: customPink,
+      borderWidth: 4,
+      borderRadius: BorderRadius.circular(30),
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 35.0),
+          child: Text('Receitas',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 35.0),
+          child: Text('Despesas',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+      ],
+      onPressed: (int newIndex) {
+        setState(() {
+          for (int index = 0; index < _isSelected.length; index++) {
+            if (index == newIndex) {
+              _isSelected[index] = true;
+            } else {
+              _isSelected[index] = false;
             }
-          });
-        },
-      ),
+          }
+        });
+      },
     );
   }
 }
